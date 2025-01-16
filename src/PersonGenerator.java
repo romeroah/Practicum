260b273363831354222ed7ef47bda4288864d474
romeroah <romeroah@mail.uc.edu>
2025-01-16 13:03:07 -0500

    dewit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonGenerator {
+
+    public static void main(String[] args)
+    {
+
+        boolean done = false;
+
+        Scanner in = new Scanner(System.in);
+        String id;
+        String fName;
+        String lName;
+        String title;
+        int birthYear;
+
+        ArrayList<String> people = new ArrayList <String> ();
+
+            do {
+                id = SafeInput.getNonZeroLenString(in, "Please enter the Person's ID");
+                System.out.println("Entered: " + id);
+                fName = SafeInput.getNonZeroLenString(in, "Please enter the Person's first name");
+                System.out.println("Entered: " + fName);
+                lName = SafeInput.getNonZeroLenString(in, "Please enter the Person's last name");
+                System.out.println("Entered: " + lName);
+                title = SafeInput.getNonZeroLenString(in, "Please enter the Person's title");
+                System.out.println("Entered: " + title);
+                birthYear = SafeInput.getRangedInt(in, "Please enter the Person's birth year", 1000, 9999);
+                System.out.println("Entered: " + birthYear);
+
+                people.add(id+", "+fName+", "+lName+", "+title+", "+birthYear);
+                done = SafeInput.getYNConfirm(in, "Close Program? [Y/N]");
+
+        }while(!done);
+    }
+}
