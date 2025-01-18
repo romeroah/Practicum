6a99c82c7ed0ff5123026a8bbf3d9f05fcf73f8a
romeroah <romeroah@mail.uc.edu>
2025-01-18 14:15:10 -0500

    Person Gen and Reader Done

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class PersonGenerator {
 
@@ -14,6 +20,7 @@ public class PersonGenerator {
         String lName;
         String title;
         int birthYear;
+        String rec;
 
         ArrayList<String> people = new ArrayList <> ();
 
@@ -29,9 +36,40 @@ public class PersonGenerator {
                 birthYear = SafeInput.getRangedInt(in, "Please enter the Person's birth year", 1000, 9999);
                 System.out.println("Entered: " + birthYear);
 
-                people.add(id+", "+fName+", "+lName+", "+title+", "+birthYear);
-                done = SafeInput.getYNConfirm(in, "Close Program? [Y/N]");
+                rec = id+", "+fName+", "+lName+", "+title+", "+birthYear;
+                people.add(rec);
+                done = SafeInput.getYNConfirm(in, "Close Program?");
 
         }while(!done);
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String person : people)
+            {
+                writer.write(person, 0, person.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // person. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
+
 }
---
+++ src/PersonReader.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonReader {
+
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try
+        {
+            // uses a fixed known path:
+            //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                int line = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    line++;
+                    // echo to screen
+                    System.out.printf("\nLine %4d %-60s ", line, rec);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
